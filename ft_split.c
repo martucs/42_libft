c977f5b7cdf2a0d4b731765b25d034fa391c381f
martucs <martalopezcab@gmail.com>
2024-01-29 20:13:51 +0100

    commit

---
+++ ft_split.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: martalop <martalop@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 17:54:56 by martalop          #+#    #+#             */
+/*   Updated: 2024/01/29 19:01:17 by martalop         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	**ft_split(char const *s, char c)
+{
+	//funcion para contar palabras/strings de 's'
+	//
+	//crear array de strings con malloc
+	//crear tantas strings como palabras haya dentro del string con malloc
+	//
+}
+
+int	main(void)
+{
+}
+
